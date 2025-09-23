2793312d3fa809e0152e07a43956be0c9a21e7a5
vishalsinghbhadauria17 <119937471+vishalsinghbhadauria17@users.noreply.github.com>
2025-09-23 17:49:04 +0530

    feat: Switch to HashRouter for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,12 +9,12 @@ import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
